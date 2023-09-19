138951dc118546b2565426a092483ea4029f6922
cocoluq <cocoluq@gmail.com>
2023-09-20 01:57:40 +0200

    lightbox successfully imported

--- lightbox.js
+++ lightbox.js
@@ -1,28 +1,28 @@
-// Get the lightbox and close button elements
+// 获取所有缩略图元素
+const galleryItems = document.querySelectorAll(".gallery-items");
+
+// 获取lightbox元素和lightbox内容元素
 const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-image");
+
+// 获取关闭按钮元素
 const closeButton = document.getElementById("close-button");
 
-// Get all the lightbox-trigger elements
-const lightboxTriggers = document.querySelectorAll(".lightbox-trigger");
-
-// Function to open the lightbox
-function openLightbox(event) {
-    const imgSrc = event.currentTarget.getAttribute("href");
-    const lightboxImg = document.getElementById("lightbox-img");
-    lightboxImg.setAttribute("src", imgSrc);
-    lightbox.style.display = "block";
-    event.preventDefault();
-}
-
-// Function to close the lightbox
-function closeLightbox() {
-    lightbox.style.display = "none";
-}
-
-// Add click event listeners to open the lightbox
-lightboxTriggers.forEach(trigger => {
-    trigger.addEventListener("click", openLightbox);
+// 添加点击事件处理程序来显示lightbox
+galleryItems.forEach(item => {
+    item.addEventListener("click", function(event) {
+        event.preventDefault();
+        // 获取点击的缩略图的原图URL
+        const fullImageSrc = this.querySelector("img").src;
+        // 设置lightbox中的<img>的src属性为原图URL
+        lightboxImg.src = fullImageSrc;
+        // 显示lightbox
+        lightbox.style.display = "block";
+    });
 });
 
-// Add click event listener to close the lightbox
-closeButton.addEventListener("click", closeLightbox);
+// 添加点击事件处理程序来关闭lightbox
+closeButton.addEventListener("click", function() {
+    // 隐藏lightbox
+    lightbox.style.display = "none";
+});
